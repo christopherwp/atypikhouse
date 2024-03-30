008faf97eef3ec1b40569a833149ee83fda838c7
gabivar <gabriela.varnum@gmail.com>
2024-03-30 08:44:34 +0100

    admin modifications - dashboard - userAdmin

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -5,33 +5,62 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
 use App\Entity\User;
-use App\Form\RegistrationFormType;
 use App\Security\EmailVerifier;
-use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use App\Form\RegistrationFormType;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
-use Symfony\Contracts\Translation\TranslatorInterface;
 use SymfonyCasts\Bundle\VerifyEmail\Exception\VerifyEmailExceptionInterface;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Form\AdminRegistrationFormType;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\Mime\Address;
 use App\Form\UserActivationType;
 
+#[Route('/admin')]
 class AdminController extends AbstractController
-{   
-    #[Route('/admin/users', name: 'app_admin_user_list')]
+{
+    private EmailVerifier $emailVerifier;
+
+    public function __construct(EmailVerifier $emailVerifier,RouterInterface $router)
+    {
+        $this->emailVerifier = $emailVerifier;
+       
+        
+    }
+    #[Route('/', name: 'app_admin_dashboard')]
+
+    public function dashboard (EntityManagerInterface $entityManager): Response
+    {
+        $usersLoca = $entityManager->getRepository(User::class)->findByRole('ROLE_LOCA');
+        $usersProprio = $entityManager->getRepository(User::class)->findByRole('ROLE_PROPRIO');
+        $usersAdmin = $entityManager->getRepository(User::class)->findByRole('ROLE_ADMIN');
+
+        return $this->render('admin/dashboard.html.twig', [
+            'controller_name' => 'AdminController',
+            'usersLoca' => $usersLoca,
+            'usersProprio' => $usersProprio,
+            'usersAdmin' => $usersAdmin,
+        ]);
+    }
+
+    #[Route('/users', name: 'app_admin_user_list')]
     public function userList(EntityManagerInterface $entityManager): Response
     {
         $users = $entityManager->getRepository(User::class)->findAll();
 
-        return $this->render('admin/userList.html.twig', [
+        return $this->render('admin/dashboard.html.twig', [
             'users' => $users,
         ]);
     }
 
-    #[Route('/admin/register', name: 'app_admin-register')]
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
+    #[Route('/register', name: 'app_admin_register')]
+    public function registerAdmin(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
     {
         $user = new User();
-        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form = $this->createForm(AdminRegistrationFormType::class, $user);
+
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
@@ -40,29 +69,33 @@ class AdminController extends AbstractController
                 $userPasswordHasher->hashPassword(
                     $user,
                     $form->get('plainPassword')->getData()
-            )
-        );
+                )
+            );
+            $user->addRole('ROLE_ADMIN');
 
-        $user->addRole('ROLE_ADMIN');
-
-        $entityManager->persist($user);
-        $entityManager->flush();
-
-        // generate a signed url and email it to the user
-        $this->emailVerifier->sendEmailConfirmation(
-            'app_verify_email',
-            $user,
-            (new TemplatedEmail())
-                ->from(new Address('atypikhouse@hotmail.com', 'atypikhouse'))
-                ->to($user->getEmail())
-                ->subject('Please Confirm your Email')
-                ->htmlTemplate('registration/confirmation_email.html.twig')
-        );
+            $entityManager->persist($user);
+            $entityManager->flush();
+    
+            // generate a signed url and email it to the user
+            $this->emailVerifier->sendEmailConfirmation(
+                'app_verify_email',
+                $user,
+                (new TemplatedEmail())
+                    ->from(new Address('atypikhouse@hotmail.com', 'atypikhouse'))
+                    ->to($user->getEmail())
+                    ->subject('Please Confirm your Email')
+                    ->htmlTemplate('registration/confirmation_email.html.twig')
+            );
+            return $this->redirectToRoute('app_admin_dashboard');
         }
-        return $this->redirectToRoute('app_admin_register');
+           
+        return $this->render('admin/adminregister.html.twig', [
+            'registrationForm' => $form,
+        ]);
+
     }
 
-    #[Route('/admin/user/{id}/activate', name: 'admin_user_activate')]
+    #[Route('/user/{id}/activate', name: 'admin_user_activate')]
     public function activateUser(Request $request, User $user, EntityManagerInterface $entityManager): Response
     {
          $form = $this->createForm(UserActivationType::class, $user);
@@ -81,13 +114,8 @@ class AdminController extends AbstractController
         'form' => $form->createView(),
         ]);
     }
-
-    
-
-
-
-
 }
+
 
 
 
---
+++ src/Form/AdminRegistrationForm.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
+class AdminRegistrationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email')
+            ->add('username')
+            ->add('plainPassword', PasswordType::class, [
+                                // instead of being set onto the object directly,
+                // this is read and encoded in the controller
+                'mapped' => false,
+                'attr' => ['autocomplete' => 'new-password'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter a password',
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 4096,
+                    ]),
+                ],
+            ])
+            // Admin : Gabriela ->
+            ->add('roles', ChoiceType::class, [
+                'mapped' => false, // Indique que ce champ n'est pas directement lié à une propriété de l'entité User
+                'choices' => [
+                    'User' => 'ROLE_USER',
+                    'Admin' => 'ROLE_ADMIN',
+                    'Proprio'=> 'ROLE_PROPRIO'
+                ],
+                'expanded' => true, // Les choix apparaissent comme des boutons radio
+                'multiple' => true, // Permet la sélection de plusieurs rôles
+            ]) // Admin : Gabriela <-
+        ;
+
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
