efa13ff8914b9a5d9f0a7311249bf805b30d623d
christopher <christopherwp@hotmail.fr>
2024-04-03 09:26:15 +0200

    api creation

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,6 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
 ];
