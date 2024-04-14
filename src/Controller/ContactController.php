2446b65c88b8c557fe21df61afa9c1138a7e5cb5
gabivar <gabriela.varnum@gmail.com>
2024-04-14 15:44:30 +0200

    securiser form contact

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -3,11 +3,21 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class ContactController extends AbstractController
 {
+    private $client;
+
+    // Injectez HttpClientInterface via le constructeur
+    public function __construct(HttpClientInterface $client)
+    {
+        $this->client = $client;
+    }
+
     #[Route('/contact', name: 'app_contact')]
     public function contact(): Response
     {
@@ -15,4 +25,31 @@ class ContactController extends AbstractController
             'controller_name' => 'ContactController',
         ]);
     }
+
+    #[Route('/submit-contact-form', name: 'submit_contact_form', methods: ['POST'])]
+    public function handleFormSubmission(Request $request): Response
+    {
+        $recaptchaResponse = $request->request->get('g-recaptcha-response');
+        $recaptchaSecret = '6LdQo7opAAAAAG2kWul1gO-wHAcj9ZS8JMrw6WCB';
+
+        $response = $this->client->request('POST', 'https://www.google.com/recaptcha/api/siteverify', [
+            'body' => [
+                'secret' => $recaptchaSecret,
+                'response' => $recaptchaResponse
+            ]
+        ]);
+
+        $results = $response->toArray();
+
+        if (true !== $results['success']) {
+            // Gérer l'échec de la vérification du CAPTCHA
+            return new Response('CAPTCHA validation failed.', Response::HTTP_FORBIDDEN);
+        }
+
+        // Traitement du formulaire après la validation du CAPTCHA
+        // Ajoutez ici votre logique de traitement du formulaire
+
+        return new Response('Form submitted successfully.');
+    }
 }
+
