2bfc5b0614166aa511029aeefa876fe56e0456be
gabivar <156909891+gabivar@users.noreply.github.com>
2024-04-15 09:31:31 +0200

    Update ContactController.php
    correction afin de sécuriser le captcha

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -23,14 +23,15 @@ class ContactController extends AbstractController
     {
         return $this->render('contact/contact.html.twig', [
             'controller_name' => 'ContactController',
+            'recaptcha_site_key' => $this->getParameter('recaptcha_site_key')                 
         ]);
     }
 
     #[Route('/submit-contact-form', name: 'submit_contact_form', methods: ['POST'])]
     public function handleFormSubmission(Request $request): Response
     {
-        $recaptchaResponse = $request->request->get('g-recaptcha-response');
-        $recaptchaSecret = '6LdQo7opAAAAAG2kWul1gO-wHAcj9ZS8JMrw6WCB';
+        $recaptchaResponse = $request->request->get('recaptcha_site_key');
+        $recaptchaSecret = $this->getParameter('recaptcha_secret_key');
 
         $response = $this->client->request('POST', 'https://www.google.com/recaptcha/api/siteverify', [
             'body' => [
@@ -43,13 +44,13 @@ class ContactController extends AbstractController
 
         if (true !== $results['success']) {
             // Gérer l'échec de la vérification du CAPTCHA
-            return new Response('CAPTCHA validation failed.', Response::HTTP_FORBIDDEN);
+            return new Response('CAPTCHA validation échouée.', Response::HTTP_FORBIDDEN);
         }
 
         // Traitement du formulaire après la validation du CAPTCHA
         // Ajoutez ici votre logique de traitement du formulaire
 
-        return new Response('Form submitted successfully.');
+        return new Response('Formulaire soumis avec succès.');
     }
 }
 
